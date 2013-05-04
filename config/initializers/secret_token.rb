f7f912a71b8c47c5a2b26fe0c57e7984e0bedc7c
JP <james@Jamess-MacBook-Air-10.local>
2013-05-04 14:25:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salesemails::Application.config.secret_token = '3369a7c21870b856b611ee3e81665e53180707480eb5c6b714312b131674a8f5a3f5c1ee8a70780e84f2f629a2d5379658d9bb081d93ae3eacdb028e4e1a3912'
